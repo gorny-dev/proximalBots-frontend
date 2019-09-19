2dd63f9321a5bd7b85222efe506957941b6336cd
Jędrzej Stasik <sjedrzej58@gmail.com>
2019-09-19 12:26:25 +0200

    Change error png and preload gif

--- src/main.js
+++ src/main.js
@@ -12,8 +12,8 @@ import VueMoment from 'vue-moment';
 //todo: wgrać własne obrazki
 Vue.use(VueLazyload, {
     preLoad: 1.3,
-    error: 'https://banner2.kisspng.com/20180404/bqw/kisspng-computer-icons-error-download-clip-art-x-mark-5ac54ca17a5a95.5409712615228796495012.jpg',
-    loading: 'https://editionsdelarose.com/wp-content/themes/edr/img/loading.gif',
+    error: 'http://public.proximalbots.ct8.pl/error.png',
+    loading: 'http://public.proximalbots.ct8.pl/preload.gif',
     attempt: 1
 });
 Vue.use(VueAxios, axios);
